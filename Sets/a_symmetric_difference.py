3429bcadcd575d5c6f6df5e75bfa5f1010a436dd
Alena Dokolina <maiya777@list.ru>
2019-01-16 14:51:36 +0300

    a_symmetric_difference.py modified - without symmetric_difference() function

--- Sets/a_symmetric_difference.py
+++ Sets/a_symmetric_difference.py
@@ -7,4 +7,13 @@ def inp():
 
 n, set1, m, set2 = inp(), set(inp()), inp(), set(inp())
 
-print(len(set1.symmetric_difference(set2)))
+#print(len(set1.symmetric_difference(set2)))
+
+count = 0
+for i in set(list(set1)+list(set2)):
+    
+    if not (i in set1 and i in set2):
+        count+=1
+        
+
+print(count)
